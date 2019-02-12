9888ea10dc778ae33afd8d5b3029aec9b883ec20
Kevin Matte <matte.kevin@gmail.com>
2019-02-12 15:38:16 -0700

    Disabling warnings

--- showdown.js
+++ showdown.js
@@ -1,12 +1,12 @@
-var mustache = require('mustache');
+const mustache = require('mustache');
 
-var showdown = require('showdown'),
+const showdown = require('showdown'),
     converter = new showdown.Converter();
 
-var fs = require('fs');
-var inputFile = process.argv[2];
-var templateFile = process.argv[3];
-var outputFile = process.argv[4];
+const fs = require('fs');
+const inputFile = process.argv[2];
+let templateFile = process.argv[3];
+let outputFile = process.argv[4];
 
 if (!outputFile) {
     outputFile = templateFile;
@@ -14,15 +14,22 @@ if (!outputFile) {
 }
 
 if (!inputFile || !outputFile) {
-    console.log(`Usage:  ${process.argv[0]} ${process.argv[1]} input.md output.template output.html`)
+    // noinspection ES6ModulesDependencies
+    // noinspection ES6ModulesDependencies
+    // noinspection ES6ModulesDependencies
+    // noinspection ES6ModulesDependencies
+    console.log(`Usage:  ${process.argv[0]} ${process.argv[1]} input.md output.template output.html`);
+    // noinspection ES6ModulesDependencies
+    // noinspection ES6ModulesDependencies
     process.exit(1);
 }
 
 
-var inputHtml = null;
-var promise;
+let inputHtml = null;
+let promise;
 
 promise = new Promise((resolve, reject) => {
+    // noinspection JSUnresolvedFunction
     fs.readFile(inputFile, 'utf8', function (err, contents) {
         if (err !== null) {
             reject(`Read error file='${inputFile}': ${err}`);
@@ -33,16 +40,17 @@ promise = new Promise((resolve, reject) => {
     });
 });
 
-var templateText = null;
+let templateText = null;
 if (templateFile) {
     promise = promise.then((t) => {
         return new Promise((resolve, reject) => {
+            // noinspection JSUnresolvedFunction
             fs.readFile(templateFile, 'utf8', function (err, contents) {
                 if (err !== null) {
                     reject(`Read error file='${templateFile}': ${err}`);
                 } else {
                     templateText = contents;
-                    var view = {
+                    const view = {
                         "body": inputHtml,
                     };
                     inputHtml = mustache.render(templateText, view);
@@ -55,6 +63,7 @@ if (templateFile) {
 
 promise = promise.then(() => {
     return new Promise((resolve, reject) => {
+        // noinspection JSUnresolvedFunction
         fs.writeFile(outputFile, inputHtml, function (err) {
             if (err !== null) {
                 reject(`Write error file='${outputFile}': ${err}`);
@@ -68,7 +77,9 @@ promise = promise.then(() => {
 
 promise.catch(function(error) {
   console.log(error);
-  process.exit(1);
+  // noinspection ES6ModulesDependencies
+    // noinspection ES6ModulesDependencies
+    process.exit(1);
 });
 
 
