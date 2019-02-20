2703a44cdf3aa09bc824c4089ecb86f5de1384e1
Kevin Matte <matte.kevin@gmail.com>
2019-02-20 15:37:49 -0700

    Cell select and Selected headers

--- paint/src/treesheet.js
+++ paint/src/treesheet.js
@@ -52,7 +52,9 @@ class Treesheet extends Component {
 
 
         this.state = {
-            hoverType: null,
+            selectedSheet: null,
+            selectedRow: null,
+            selectedCol: null,
             spreadsheet: this.createSpreadsheet(props),
         }
     }
@@ -159,42 +161,33 @@ class Treesheet extends Component {
         );
     }
 
-    handleHover = (sheetName) => this.setState({hoverType: sheetName});
-
     renderSheet(sheetName) {
+        let {selectedSheet, selectedRow, selectedCol} = this.state;
+
         let iCell = 0;
         let spreadsheet = this.state.spreadsheet;
         let sheet = spreadsheet.sheetsByName[sheetName];
-        let sheetStyle = {...sheet.style};
 
         let cells = [];
 
-        let {hoverType} = this.state;
-        let showHeader = sheetName === hoverType || (!hoverType && spreadsheet.sheetNames[0] === sheetName);
-        if (showHeader) {
+        // Render column header labels
+        let isSelectedSheet = sheetName === selectedSheet || (!selectedSheet && spreadsheet.sheetNames[0] === sheetName);
+        if (isSelectedSheet) {
+            // 1st column
             let style = Object.assign({}, this.rowHeaderStyle, {gridRow: 1, gridColumn: 1});
             cells.push((
                 <div key={++iCell} style={style}>
                     {sheet.typeName}
                 </div>
             ));
-        }
 
-        let widths = sheet.type.columns.reduce((dest, col) => {
-            dest.push(this.gridWidth, col.width);
-            return dest;
-        }, []);
-        let indentWidth = sheet.indent > 0 ? `${this.gridWidth} ${sheet.indent * this.indentPixels}px` : '0px 0px';
-        sheetStyle.gridTemplateColumns = `${this.headerColumnWidth}px ${indentWidth} ` + widths.join(" ");
-
-        // Render header
-        if (showHeader) {
+            // column labels
             let iCol = 5;
-            sheet.type.columns.every((column) => {
+            sheet.type.columns.every((column, cellCol) => {
                 let style = {...this.columnHeaderStyle, gridRow: 1, gridColumn: iCol++};
                 iCol++; // Grid
                 return cells.push((
-                    <div key={++iCell} style={style}>
+                    <div key={++iCell} style={style} className={cellCol === selectedCol ? "selectedHeader" : ""}>
                         {column.label}
                     </div>
                 ));
@@ -202,26 +195,42 @@ class Treesheet extends Component {
         }
 
         let iRow = 1;
-        spreadsheet.rows.every(row => {
+        spreadsheet.rows.every((row, cellRow) => {
             iRow++;
             if (row.sheetName === sheetName) {
                 let style = {...this.rowHeaderStyle, gridRow: iRow, gridColumn: 1};
                 cells.push((
-                    <div key={++iCell} style={style}>
+                    <div key={++iCell} style={style} className={cellRow === selectedRow ? "selectedHeader" : ""}>
                         {sheet.typeName}
                     </div>
                 ));
 
-                let iCol = 5;
-                row.columns.every((column) => {
-                    let style = {...this.valueStyle, gridRow: iRow, gridColumn: iCol++};
+                let iCol = 4;
+                row.columns.every((column, cellCol) => {
+                    let hasSelectedCol = selectedCol !== null
+                        && sheetName === selectedSheet
+                        && (selectedCol === cellCol || selectedCol+1 === cellCol);
+                    if (hasSelectedCol) {
+                        cells.push((
+                        <div
+                            key={++iCell}
+                            style={{gridRow: iRow, gridColumn: iCol}}
+                            className="selectedGrid"
+                        >
+                        </div>));
+                    }
                     iCol++; // Skip grid
+
+                    let style = {...this.valueStyle, gridRow: iRow, gridColumn: iCol++};
+                    let cellClasses = (cellRow === selectedRow && selectedCol === cellCol) ?
+                        "Cell selectedCell" : "Cell yellowOnHover";
                     return cells.push((
                         <div
                             key={++iCell}
                             style={style}
-                            className="Cell yellowOnHover"
-                            onMouseOver={() => this.handleHover(sheetName)}
+                            className={cellClasses}
+                            onMouseOver={() => this.handleCellHover(sheetName)}
+                            onMouseUp={() => this.handleCellSelect(sheetName, cellRow, cellCol)}
                         >
                             {column}
                         </div>
@@ -231,9 +240,33 @@ class Treesheet extends Component {
             return cells;
         });
 
+        // Render grid div
+        let widths = sheet.type.columns.reduce((dest, col) => {
+            dest.push(this.gridWidth, col.width);
+            return dest;
+        }, []);
+        let indentWidth = sheet.indent > 0 ? `${this.gridWidth} ${sheet.indent * this.indentPixels}px` : '0px 0px';
+        let sheetStyle = {...sheet.style};
+        sheetStyle.gridTemplateColumns = `${this.headerColumnWidth}px ${indentWidth} ` + widths.join(" ");
         return <div id={sheetName} key={sheetName} style={sheetStyle} className="Spreadsheet">
             {cells}
         </div>;
+    }
+
+    handleCellSelect(sheetName, cellRow, cellCol) {
+        let {selectedRow, selectedCol} = this.state;
+        if (cellRow != null && cellRow === selectedRow && selectedCol === cellCol)
+            this.setState({selectedSheet: null, selectedRow: null, selectedCol: null});
+        else
+            this.setState({selectedSheet: sheetName, selectedRow: cellRow, selectedCol: cellCol});
+    }
+
+    handleCellHover(sheetName) {
+        let {selectedRow} = this.state;
+        if (selectedRow === null) {
+            this.setState({selectedSheet: sheetName});
+            console.log('hover');
+        }
     }
 }
 
