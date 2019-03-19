d7109a467b1b0bedf59254338da250c88dfd4d85
Kevin Matte <matte.kevin@gmail.com>
2019-03-19 15:25:32 -0600

    paint/src/components/Treesheet/Datasheet.js: Converted from Component to function

--- paint/src/components/Treesheet/Datasheet.js
+++ paint/src/components/Treesheet/Datasheet.js
@@ -1,6 +1,6 @@
 /* Copyright (C) 2019 Kevin Matte - All Rights Reserved */
 
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
 
 import {getGridCellStyle, getValueByPath} from "../../general/Utils";
@@ -12,42 +12,59 @@ import TempValues from "../../redux/actions/tempValues";
 import {connect} from "react-redux";
 
 
-class Datasheet extends Component {
+function Datasheet(props) {
+    let {setTempValueByPath, name, selectedRow} = props;
 
-    setTempValueByPath = (field, value) => this.props.setTempValueByPath(`${this.props.name}/${field}`, value);
+    let setSheetPropValue = (field, value) => setTempValueByPath(`${name}/${field}`, value);
 
-    handleCellHover(sheetName) {
-        let {selectedRow} = this.props;
+    let setEditValue = value => setSheetPropValue('editValue', value);
+
+    let saveEditValue = (cellRow, cellCol) => {
+        let {treesheetModel, editValue} = props;
+
+        let row = treesheetModel.openRows[cellRow];
+        let sheet = treesheetModel.sheetsByName[row.sheetName];
+        let columnPath = sheet.type.columns[cellCol].path;
+        columnPath = Array.isArray(columnPath) ? columnPath : [columnPath];
+        let path = [...row.path, ...columnPath];
+        props.setValueByPath(path, editValue);
+        row.values[cellCol] = editValue;
+    };
+
+    let handleCellHover = (sheetName) => {
         if (selectedRow === null) {
-            this.setTempValueByPath('selectedSheetName', sheetName);
+            setSheetPropValue('selectedSheetName', sheetName);
         }
-    }
+    };
 
-    handleCellSelect(sheetName, cellRow, cellCol) {
-        let {selectedRow, selectedCol, treesheetModel} = this.props;
+    let handleCellSelect = (sheetName, cellRow, cellCol) =>
+    {
+        let {selectedRow, selectedCol, treesheetModel} = props;
         if (cellRow !== selectedRow || selectedCol !== cellCol) {
             if (selectedRow !== null && selectedCol != null) {
-                this.saveEditValue(selectedRow, selectedCol);
+                saveEditValue(selectedRow, selectedCol);
             }
             if (cellRow != null && cellCol != null) {
                 let row = treesheetModel.openRows[cellRow];
                 let value = row.values[cellCol];
-                this.setEditValue(value);
-                this.setTempValueByPath('selectedSheetName', sheetName);
-                this.setTempValueByPath('selectedRow', cellRow);
-                this.setTempValueByPath('selectedCol', cellCol);
+                setEditValue(value);
+                setSheetPropValue('selectedSheetName', sheetName);
+                setSheetPropValue('selectedRow', cellRow);
+                setSheetPropValue('selectedCol', cellCol);
 
                 let sheet = treesheetModel.sheetsByName[row.sheetName];
                 let columnPath = sheet.type.columns[cellCol].path;
                 columnPath = Array.isArray(columnPath) ? columnPath : [columnPath];
                 let path = [...row.path, ...columnPath];
-                this.setTempValueByPath('selectedPath', path);
+                setSheetPropValue('selectedPath', path);
             }
         }
-    }
+    };
 
-    renderSheets() {
-        let {treesheetModel, selectedSheetName, selectedRow, selectedCol, editValue, gridSpacingWidth, indentPixels, rowHeight} = this.props;
+    let renderSheets = () =>
+    {
+        let {treesheetModel, selectedSheetName, selectedRow,
+            selectedCol, editValue, gridSpacingWidth, indentPixels, rowHeight} = props;
 
         let cellsBySheet = treesheetModel.sheetNames.reduce(
             (dst, sheetName) => {
@@ -73,7 +90,7 @@ class Datasheet extends Component {
                         style={getGridCellStyle(cellRow + 1, 2)}
 
                     >
-                        <button onClick={() => this.toggleOpen(row)}>{symbol}</button>
+                        <button onClick={() => toggleOpen(row)}>{symbol}</button>
                     </div>));
 
             }
@@ -108,7 +125,7 @@ class Datasheet extends Component {
                     cell = <Cell
                         doEdit={isSelected}
                         value={editValue}
-                        setValue={value => this.setEditValue(value)}
+                        setValue={value => setEditValue(value)}
                     />;
                 } else {
                     cell = <Cell value={value}/>;
@@ -118,8 +135,8 @@ class Datasheet extends Component {
                         key={++iCell}
                         style={getGridCellStyle(cellRow + 1, iCol++)}
                         className={cellClasses}
-                        onMouseOver={() => this.handleCellHover(sheetName)}
-                        onMouseUp={() => this.handleCellSelect(sheetName, cellRow, cellCol)}
+                        onMouseOver={() => handleCellHover(sheetName)}
+                        onMouseUp={() => handleCellSelect(sheetName, cellRow, cellCol)}
                     >
                         {cell}
                     </div>
@@ -134,7 +151,7 @@ class Datasheet extends Component {
             let sheet = treesheetModel.sheetsByName[sheetName];
             // Render grid div
             let widths = sheet.type.columns.reduce((dest, col) => {
-                dest.push(`${this.props.gridSpacingWidth}px`, col.width);
+                dest.push(`${gridSpacingWidth}px`, col.width);
                 return dest;
             }, []).join(" ");
             let indentWidth = `${gridSpacingWidth}px ${sheet.path.length * indentPixels}px`;
@@ -152,50 +169,32 @@ class Datasheet extends Component {
                     {cellsBySheet[sheetName]}
                 </div>);
         });
-    }
-
-    render() {
-        return (
-            <div className="max_size overflowAuto">
-                <div
-                    className="SpreadsheetOverlay"
-                >
-                    {this.renderSheets()}
-                </div>
-            </div>
-        );
-    }
-
-    setEditValue = value => this.setTempValueByPath('editValue', value);
-
-    toggleOpen = (row) => {
-        row.isOpen = !row.isOpen;
-        this.props.treesheetModel.updateSpreadsheetOpenRows();
-        this.handleCellSelect(null, null, null);
-        this.dropSelection();
     };
 
-    dropSelection() {
-        this.setTempValueByPath('updated', this.props.updated + 1);
-        this.setTempValueByPath('selectedSheetName', null);
-        this.setTempValueByPath('selectedRow', null);
-        this.setTempValueByPath('selectedCol', null);
-        this.setTempValueByPath('selectedPath', null);
-    }
+    let toggleOpen = (row) => {
+        row.isOpen = !row.isOpen;
+        props.treesheetModel.updateSpreadsheetOpenRows();
+        handleCellSelect(null, null, null);
+        dropSelection();
+    };
 
-    saveEditValue(cellRow, cellCol) {
-        let {treesheetModel, editValue} = this.props;
+    let dropSelection = () => {
+        setSheetPropValue('updated', props.updated + 1);
+        setSheetPropValue('selectedSheetName', null);
+        setSheetPropValue('selectedRow', null);
+        setSheetPropValue('selectedCol', null);
+        setSheetPropValue('selectedPath', null);
+    };
 
-        let row = treesheetModel.openRows[cellRow];
-        let sheet = treesheetModel.sheetsByName[row.sheetName];
-        let columnPath = sheet.type.columns[cellCol].path;
-        columnPath = Array.isArray(columnPath) ? columnPath : [columnPath];
-        let path = [...row.path, ...columnPath];
-        this.props.setValueByPath(path, editValue);
-        row.values[cellCol] = editValue;
-    }
-
-
+    return (
+        <div className="max_size overflowAuto">
+            <div
+                className="SpreadsheetOverlay"
+            >
+                {renderSheets()}
+            </div>
+        </div>
+    );
 }
 
 Datasheet.defaultProps = {
